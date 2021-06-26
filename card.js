18974bcbb26b2bb6092c549c39b59ae865e824f5
Hormard <Hormard@mail.ru>
2021-06-26 14:16:52 +0300

    Update

--- card.js
+++ card.js
@@ -2,9 +2,9 @@ export function Card(title, description){
     this.title = title;
     this.description = description;
     this.getHtml = function(){
-        return `<div class="card border-success mb-3" style="max-width: 18rem;">
+        return `<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
                     <div class="card-header">${this.getTime()}</div>
-                    <div class="card-body text-success">
+                    <div class="card-body">
                         <h5 class="card-title">${this.title}</h5>
                         <p class="card-text">${this.description}</p>
                     </div>
